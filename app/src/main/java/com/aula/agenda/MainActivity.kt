117dcb9501077ef354b59e5666611ff74972d2aa
Celso Fernando Paes <ceferdo@gmail.com>
2020-12-19 12:03:20 -0300

    criacao menu

--- app/src/main/java/com/aula/agenda/MainActivity.kt
+++ app/src/main/java/com/aula/agenda/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.aula.agenda
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Menu
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val contatos
+        val contatos = arrayOf("Maria", "José", "Carlos")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contatos)
+
+        toolbar.setTitleTextColor(Color.WHITE)
+        setSupportActionBar(toolbar)
+
+        var listaContatos = lista
+        listaContatos.setAdapter(adapter);
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+
 }
